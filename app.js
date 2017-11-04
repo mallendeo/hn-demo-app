bad567c8723bb1965f89fdc763ebecfcaefeccf0
Mauricio Allende <mauricio.allende.o@gmail.com>
2017-11-04 16:17:11 -0300

    add HN background daemon

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const pug = require('pug')
 const app = express()
 const mongoose = require('mongoose')
 
+const hn = require('./lib/hn')
+
 const {
   PORT = 3000,
   MONGO_DB_PORT = 27017,
@@ -14,6 +16,22 @@ const {
 mongoose.Promise = Promise
 mongoose.connect(MONGO_DB_URL, { useMongoClient: true })
 const db = mongoose.connection
+
+// HN background daemon
+;(async () => {
+  const min = n => n * 60000
+
+  async function retrieveAndSavePosts () {
+    try {
+      const posts = await hn.getPosts()
+    } catch (e) {
+      console.error(`There was an error retrieving the posts.`)
+    }
+  }
+  
+  retrieveAndSavePosts()
+  setInterval(retrieveAndSavePosts, min(60))
+})()
 
 app.set('view engine', 'pug')
 app.use(express.static(`${__dirname}/public`))
