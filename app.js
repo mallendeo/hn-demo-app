2358c1a6b073c1fafdb1322c6d77ca031c5506cb
Mauricio Allende <mauricio.allende.o@gmail.com>
2017-11-04 18:47:25 -0300

    show stories only and move toggle mock data with env variables

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const postRouter = require('./components/post/router')
 const {
   PORT = 3000,
   MONGO_DB_PORT = 27017,
+  MOCK_DATA = false,
   MONGO_DB_URL = `mongodb://localhost:${MONGO_DB_PORT}`
 } = process.env
 
@@ -28,7 +29,11 @@ db.on('error', e => console.error(`Connection error: ${e}`))
 
   async function retrieveAndSavePosts () {
     try {
-      const posts = await hn.getPosts()
+      const posts = await hn.getPosts({
+        storiesOnly: true,
+        useMockData: MOCK_DATA
+      })
+
       await post.insertPosts(posts)
     } catch (e) {
       console.error(`HN daemon error: ${e.message}`)
