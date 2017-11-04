d1f14556bc55e564f904f6d99c49408fc9e2f4c8
Mauricio Allende <mauricio.allende.o@gmail.com>
2017-11-04 15:49:15 -0300

    add mongo connection and env variables

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const pug = require('pug')
 const app = express()
 const mongoose = require('mongoose')
 
-const { PORT = 3000 } = process.env
+const {
+  PORT = 3000,
+  MONGO_DB_PORT = 27017,
+  MONGO_DB_URL = `mongodb://localhost:${MONGO_DB_PORT}`
+} = process.env
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_DB_URL, { useMongoClient: true })
+const db = mongoose.connection
 
 app.set('view engine', 'pug')
 app.use(express.static(`${__dirname}/public`))
